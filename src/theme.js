fd50b930eade6fdb1201d27ce78626ce37f28fe3
Herohtar <belac1186@gmail.com>
2018-09-22 19:02:54 -0500

    Changed theme to match original site

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,10 @@
-import { brown, grey, deepOrange, deepPurple, red } from '@material-ui/core/colors';
+import { blueGrey, deepOrange } from '@material-ui/core/colors';
 
 export default {
   palette: {
-    type: 'light',
-    primary: deepPurple,
-    secondary: deepOrange,
-    error: red,
-    background: {
-      paper: deepPurple[50],
-      default: deepPurple[100],
-      order: deepPurple[100],
-    },
+    type: 'dark',
+    primary: blueGrey,
+    secondary: blueGrey,
+    error: deepOrange,
   },
 }

cb633acdb0fc3e23726e77a2008468a4819f54c8
Herohtar <belac1186@gmail.com>
2018-09-27 14:07:04 -0500

    Added link to blog

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -23,5 +23,6 @@ export default withStyles(styles)(({ classes }) => (
     )} />
     <Typography variant="display1" paragraph>Welcome!</Typography>
     <Typography variant="body1" paragraph>This site has nothing on it... yet.</Typography>
+    <Typography variant="body2">Check out the <Link to="/blog">blog</Link>.</Typography>
   </div>
 ))

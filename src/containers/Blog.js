77d9b7516b87463d66ddde9eb41e88d68f84701d
Herohtar <belac1186@gmail.com>
2018-09-27 14:39:19 -0500

    Test to see if match is populated

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -20,7 +20,7 @@ const styles = theme => ({
   },
 })
 
-export default withStyles(styles)(({ classes }) => (
+export default withStyles(styles)(({ classes, match }) => (
   <div className={classes.root}>
     <SiteData render={({title}) => (
         <Head title={title} />
@@ -30,7 +30,7 @@ export default withStyles(styles)(({ classes }) => (
         posts.map(post => (
           <Grid item xs="auto" key={post.path}>
             <Paper className={classes.post}>
-              <Typography variant="headline" component={Link} to={post.path} className={classes.link}>{post.title}</Typography>
+              <Typography variant="headline" component={Link} to={post.path} className={classes.link}>{match}</Typography>
               <Typography variant="subheading" paragraph><Moment date={post.date} format="dddd, MMMM Do, YYYY" /></Typography>
               <Typography variant="body1" color="textSecondary" component={ReactMarkdown} source={post.body} paragraph />
             </Paper>

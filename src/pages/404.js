594b6f0e35b8cb92768f6943f8c29069aff756c0
Herohtar <belac1186@gmail.com>
2018-12-21 18:06:48 -0600

    Improved 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { SiteData, Head } from 'react-static'
 //
+import Typography from '@material-ui/core/Typography'
 
 export default class extends React.Component {
   state = {
@@ -21,7 +23,13 @@ export default class extends React.Component {
   render() {
     return this.state.ready ? (
       <div>
-        <h1>404 - Oh no's! We couldn't find that page :(</h1>
+        <SiteData>
+          {({title}) => (
+            <Head title={`404 - Page Not Found - ${title}`} />
+          )}
+        </SiteData>
+        <Typography variant="h3" align="center" paragraph>404</Typography>
+        <Typography variant="h4" align="center">The page you requested could not be found.</Typography>
       </div>
     ) : null
   }

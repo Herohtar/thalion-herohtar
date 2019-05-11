10f274b4c5e83fae5861a080cc00338e109e1436
Herohtar <belac1186@gmail.com>
2019-05-11 17:41:21 -0500

    Fix createMuiTheme import

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import createMuiTheme from '@material-ui/core/createMuiTheme'
+import { createMuiTheme } from '@material-ui/core/styles'
 import { blueGrey, deepOrange, grey } from '@material-ui/core/colors';
 
 const theme = createMuiTheme({

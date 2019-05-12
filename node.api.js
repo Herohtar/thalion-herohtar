b73717d62415e5585bf9fce80fc915a14d08e7fd
Herohtar <belac1186@gmail.com>
2019-05-11 20:39:09 -0500

    Use beforeRenderToHtml instead of beforeRenderToElement

--- node.api.js
+++ node.api.js
@@ -4,12 +4,12 @@ import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
 import theme from './src/theme'
 
 export default () => ({
-  beforeRenderToElement: (App, { meta }) => props => {
+  beforeRenderToHtml: (App, { meta }) => {
     meta.muiSheets = new ServerStyleSheets()
 
     return meta.muiSheets.collect(
       <ThemeProvider theme={theme}>
-        <App {...props} />
+        {App}
       </ThemeProvider>
     )
   },

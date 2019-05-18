b3ac461de750a18b98453f79abc1f5bc91966e0a
Herohtar <belac1186@gmail.com>
2019-05-17 20:05:44 -0500

    Use Luxon component instead of Moment

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,7 +6,7 @@ import { makeStyles } from '@material-ui/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import Moment from 'react-moment'
+import Luxon from '../components/Luxon'
 import ReactMarkdown from 'react-markdown'
 
 const useStyles = makeStyles(theme => ({
@@ -35,7 +35,7 @@ export default () => {
           <Grid item xs="auto" key={post.path}>
             <Paper className={classes.post}>
               <Typography variant="h5" component={Link} to={post.path} className={classes.link}>{post.title}</Typography>
-              <Typography variant="subtitle1" paragraph><Moment date={post.date} format="dddd, MMMM Do, YYYY" /></Typography>
+              <Typography variant="subtitle1" paragraph><Luxon date={post.date} /></Typography>
               <Typography variant="body2" color="textSecondary" component={ReactMarkdown} source={post.body} paragraph />
             </Paper>
           </Grid>
